1cc5e89c7dcbc63c546fd97f4accab2b6b8e1f71
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 13:08:14 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
-# Write your methods here
+def loop_message_five_times(message)
+  counter = 0
+  while counter <= 5 do
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times(message, n)
+  counter = 0 
+  while counter <= n do
+    puts message
+    counter += 1
+  end
+end
+
+def output_array(array)
+  index = 0
+  while index < array.length do
+    puts array[index]
+    index += 1
+  end
+end
+
+def return_string_array(array)
+  string_array = []
+  index = 0
+  while index < array.length do
+    string_array.push(array[index].to_s)
+    index += 1
+  end
+  string_array
+end
+    
+
+  
