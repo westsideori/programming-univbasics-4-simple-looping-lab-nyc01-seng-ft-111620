05bd9b12431fe50df0d84e9582c63b3294e51e72
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 12:57:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,3 @@
-def loop_message_five_times(message)
-  counter = 0
-  while counter <= 5 do
-    puts message
-  end
-end
 
-def loop_message_n_times(message, n)
-  counter = 0 
-  while counter <= n do
-    puts message
-  end
-end
 
   
